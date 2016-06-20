427ed513498c4dfb81d002f73e8413e629d1f51c
Kyle Ladd <kladd6@gmail.com>
2016-06-20 12:26:10 -0400

    Recursive flattening and json to table

--- src/jquery.jsontotable.js
+++ src/jquery.jsontotable.js
@@ -1,17 +1,12 @@
 // var flatten = require('flat');
 (function($) {
   $.jsontotable = function(data, options) {
-    var settings = $.extend({
-      id: null, // target element id
-      header: true,
-      className: null
-    }, options);
-
-    options = $.extend(settings, options);
+    ///////////Start Declaring Functions
     // Underscorejs
     var isArray = Array.isArray || function(obj) {
       return Object.prototype.call(obj) === '[object Array]';
     };
+
     // https://gist.github.com/penguinboy/762197
     var flattenObject = function(ob) {
       var toReturn = {};
@@ -20,7 +15,8 @@
         if (!ob.hasOwnProperty(i)) {
           continue;
         }
-          if ((typeof ob[i]) === 'object' && ob[i] !== null) {
+        //Make sure it is not a list, json to table will handle lists
+          if ((typeof ob[i]) === 'object' && ob[i] !== null && !isArray(ob[i])) {
           var flatObject = flattenObject(ob[i]);
           for (var x in flatObject) {
             if (!flatObject.hasOwnProperty(x)) {
@@ -35,95 +31,6 @@
       }
       return toReturn;
     };
-    var obj = data;
-    if (typeof obj === "string") {
-      obj = $.parseJSON(obj);
-    }
-    if(!isArray(obj)){
-      obj = [obj];
-    }
-    // Flatten
-    // obj = flattenObject(obj);
-    for(var index in obj){
-      obj[index] = flattenObject(obj[index]);
-    }
-    console.log(obj);
-    if (options.id && obj.length) {
-
-      var i, row;
-      var table = $("<table></table>");
-
-      if (options.className) {
-        table.addClass(options.className);
-      }
-
-      $.fn.appendTr = function(rowData, isHeader) {
-        var frameTag = isHeader ? "thead" : "tbody";
-        var rowTag = isHeader ? "th" : "td";
-        var rowi,key,cellObj,cell,j;
-
-        /* if rowData is object, set the key and value as tr's properties */
-        if ($.isPlainObject(rowData) && rowData._data) {
-          row = '<tr';
-
-          for (rowi in rowData) {
-            if (rowi !== '_data') {
-              row += ' ' + rowi + '="' + rowData[rowi] + '"';
-            }
-          }
-          row += '></tr>';
-          rowData = rowData._data;
-
-        } else {
-          row = "<tr></tr>";
-        }
-
-        row = $(row);
-
-        for (key in rowData) {
-          cellObj = rowData[key];
-
-          if (typeof cellObj !== "function") { /* ADDED: this wrapper to account for people bootstrapping the ECMA Array model otherwise functions get converted to strings and show up in the object list / output */
-
-            cell = '';
-
-            /* if cellObj is object, set the key and value as cell's properties */
-            if ($.isPlainObject(cellObj) && cellObj._data) {
-              cell = "<" + rowTag;
-
-              for (j in cellObj) {
-                if (j !== '_data') {
-                  cell += ' ' + j + '="' + cellObj[j] + '"';
-                }
-              }
-
-              cellObj = cellObj._data;
-
-              cell += '>' + cellObj + "</" + rowTag + ">";
-
-            } else {
-              cell = "<" + rowTag + ">" + cellObj + "</" + rowTag + ">";
-            }
-
-            row.append(cell);
-          }
-        }
-
-        if (isHeader) { /* ADDED: IF/ELSE to eliminate repetitive TBODY tags for every row */
-          $(this).append($("<" + frameTag + "></" + frameTag + ">").append(row));
-
-        } else {
-          var tbody = $(this).find("tbody");
-          if (tbody.length === 0) {
-            tbody = $(this).append("<tbody></tbody>");
-          }
-
-          tbody.append(row); //always append data rows to the first tbody tag
-        }
-
-        return this;
-      };
-
       // from http://stackoverflow.com/questions/122102/what-is-the-most-efficient-way-to-clone-an-object
       var clone = function (obj) {
         if(obj == null || typeof(obj) !== "object") {
@@ -138,38 +45,100 @@
         }
         return temp;
       };
+      var createRow = function(rowData, isHeader){
+        var frameTag = isHeader ? "thead" : "tbody";
+        var rowTag = isHeader ? "th" : "td";
+        var rowi,key,cellObj,cell,j;
+        var row = "<tr>";
 
-      var dictType = false, headerObj = {}, key = null;
-      if (options.header) {
-        headerObj = obj[0]._data ? clone(obj[0]._data) : clone(obj[0]);
-        
-        if (headerObj.toString() === "[object Object]") { // data type is dictonary
-          dictType = true;
-          for (key in headerObj) { headerObj[key] = key; }
-        }
+        for (key in rowData) {
+          cellObj = rowData[key];
 
-        table.appendTr(headerObj, true);
-      }
-
-      /**
-      /* MODIFIED: options.header ? 1 : 0
-      /* to eliminate duplicating header as the first row of data 
-      **/
-      for (i = ((obj[0]._data ||  !dictType) && options.header ? 1 : 0); i < obj.length; i++) {
-        if (dictType && headerObj) {
-          var bodyItem = {};
-          for (key in headerObj) {
-            bodyItem[key] = (obj[i] && obj[i][key] != null) ? obj[i][key] : "";
+          if (typeof cellObj !== "function") { /* ADDED: this wrapper to account for people bootstrapping the ECMA Array model otherwise functions get converted to strings and show up in the object list / output */
+            if(isArray(cellObj)){
+              cellObj = createJSONtable(cellObj);
+            }
+            cell = "<" + rowTag + ">" + cellObj + "</" + rowTag + ">";
+            row += cell;
           }
-          table.appendTr(bodyItem, false);
         }
-        else {
-          table.appendTr(obj[i], false);
-        }
+        row += "</tr>";
+        return row;
       }
+  var createJSONtable = function(data){
+      var table = "";
+      var obj = data;
+      // if (typeof obj === "string") {
+      //   obj = $.parseJSON(obj);
+      // }
+      // if(!isArray(obj)){
+      //   obj = [obj];
+      // }
+      // Flatten
+      for(var index in obj){
+        obj[index] = flattenObject(obj[index]);
+      }
+      // console.log(obj);
+      if (options.id && obj.length) {
 
-      $(options.id).append(table);
+        var i, row;
+        table = "<table class=\"" + options.className + "\">";
+
+        var dictType = false, headerObj = {}, key = null;
+        if (options.header) {
+          table += "<thead>";
+          headerObj = obj[0]._data ? clone(obj[0]._data) : clone(obj[0]);
+          
+          if (headerObj.toString() === "[object Object]") { // data type is dictonary
+            dictType = true;
+            for (key in headerObj) { headerObj[key] = key; }
+          }
+
+          table += createRow(headerObj, true);
+          table += "</thead>";
+        }
+
+        /**
+        /* MODIFIED: options.header ? 1 : 0
+        /* to eliminate duplicating header as the first row of data 
+        **/
+        table += "<tbody>";
+        for (i = ((obj[0]._data ||  !dictType) && options.header ? 1 : 0); i < obj.length; i++) {
+          if (dictType && headerObj) {
+            var bodyItem = {};
+            for (key in headerObj) {
+              bodyItem[key] = (obj[i] && obj[i][key] != null) ? obj[i][key] : "";
+            }
+            table += createRow(bodyItem, false);
+          }
+          else {
+            table += createRow(obj[i], false);
+          }
+        }
+        table += "</tbody>";
+        table += "</table>";
+      }
+      return table;
     }
+
+    ///////////End Declaring Functions
+
+    var settings = $.extend({
+      id: null, // target element id
+      header: true,
+      className: null
+    }, options);
+
+    options = $.extend(settings, options);
+    if (typeof data === "string") {
+      data = $.parseJSON(data);
+    }
+    if(!isArray(data)){
+      data = [data];
+    }
+    var result = createJSONtable(data);
+    console.log(result);
+    $(options.id).append(result);
 
     return this;
   };
